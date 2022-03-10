15dbf2e0606e4dcb951e365f54fc6a63ed8a646d
Noah Vendrig <noahvendrig@gmail.com>
2022-03-10 14:12:54 +1100

    fixed selection sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,21 +1,20 @@
-import sys
-A = [64, 25, 12, 22, 11]
- 
-# Traverse through all array elements
-for i in range(len(A)):
-     
-    # Find the minimum element in remaining
-    # unsorted array
-    min_idx = i
-    for j in range(i+1, len(A)):
-        if A[min_idx] > A[j]:
-            min_idx = j
-             
-    # Swap the found minimum element with
-    # the first element       
-    A[i], A[min_idx] = A[min_idx], A[i]
- 
-# Driver code to test above
-print ("Sorted array")
-for i in range(len(A)):
-    print("%d" %A[i]),
+
+def SelectionSort(arr):
+    # Traverse through all array elements
+    for i in range(len(arr)):
+        # Find the minimum element in remaining unsorted array
+        min = i
+        for j in range(i+1, len(arr)):
+            if arr[min] > arr[j]:
+                min = j
+
+        # Swap the found minimum element with
+        # the first element
+        arr[i], arr[min] = arr[min], arr[i]
+    return arr
+
+
+# Test the code
+arr = [111, 73, 230, 2, 15, 24]  # test case
+print("Sorted array")
+print(SelectionSort(arr))
